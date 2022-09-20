88c76d189cf2233ebb69cd3ad398dbab9f979000
victor099 <victor.pereira@shipay.com.br>
2022-09-20 13:03:49 -0300

    feat(3): implemeting cleaning function

--- garbage_destroyer/app.py
+++ garbage_destroyer/app.py
@@ -1,16 +1,24 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import os
+from datetime import datetime, timedelta
 
 from settings import config
 
 
-def my_function():
-    print('test-function')
+def clean():
+    for item in os.scandir(config['Cleaning']['dir']):
+        last_access = datetime.fromtimestamp(os.path.getatime(item))
+        delta = datetime.now() - last_access
+        
+        if os.path.isfile(item) and delta.days > int(config['Cleaning']['time_diff_in_days']):
+            os.remove(item)
+            
 
 
 def schedule():
     scheduler = BlockingScheduler()
     params = config['Scheduler']
-    scheduler.add_job(my_function, 'cron', **params)
+    scheduler.add_job(clean, 'cron', **params)
     scheduler.start()
 
 
