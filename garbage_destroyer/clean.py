be8cf788eba4d1cbefb4cc09e44714a47e709aa3
Lnvictor <vh141299@gmail.com>
2022-11-17 02:04:45 -0300

    feat: lint adjusts

--- garbage_destroyer/clean.py
+++ garbage_destroyer/clean.py
@@ -12,6 +12,6 @@ def clean(time_diff, ext, dir):
     for item in glob.glob(path):
         last_access = datetime.fromtimestamp(os.path.getatime(item))
         delta = datetime.now() - last_access
-        
+
         if os.path.isfile(item) and delta.days > time_diff:
             os.remove(item)
--- tests/test_clean.py
+++ tests/test_clean.py
@@ -6,7 +6,7 @@ from garbage_destroyer.clean import clean
 def test_should_clean_files_with_extension(os_mock, glob_mock, config_mock):
     config_mock.return_value = {'Scheduler': {'hour': '*', 'minute': '*', 'second': '*'},
                                 'Cleaning': {'dir': '/Users/victorpereira/Downloads', 'time_diff_in_days': '1',
-                                'file_extension': 'jpg'}}
+                                             'file_extension': 'jpg'}}
     glob_mock.return_value = ['alou.jpg', 'tchau.jpg']
     os_mock.path.getatime.return_value = (datetime.now() - timedelta(days=5)).timestamp()
     os_mock.path.isfile.return_value = True
